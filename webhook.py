b621bbe116e2bfb92a498c09c48e112cfd14f79f
Marcelo Bittencourt <megabit@gmail.com>
2020-04-20 23:09:22 -0300

    fix os.environ.get

--- webhook.py
+++ webhook.py
@@ -9,18 +9,18 @@ from flask import Flask, request, abort, jsonify
 LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
 logging.basicConfig(level=LOGLEVEL)
 
-TELEGRAM_BOT_TOKEN = os.environ('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.environ('TELEGRAM_CHAT_ID')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 bot_url = 'https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN + '/sendMessage'
 
-SC_API_TOKEN = os.environ('SC_API_TOKEN')
+SC_API_TOKEN = os.environ.get('SC_API_TOKEN')
 
 #SC_USER = os.environ('SC_USER')
 #SC_APIKEY = os.environ('SC_APIKEY')
 #webhook_token_str = '%s%s' % (SC_USER, SC_APIKEY)
 #logging.info('Text to Hash: %s' % webhook_token_str)
 #webhook_token = hashlib.md5(webhook_token_str.encode())
-#logging.info('Autorized Token: %s' % webhook_token.hexdigest())
+logging.info('Autorized Token: %s' % webhook_token.hexdigest())
 
 app = Flask(__name__)
 turl = pycurl.Curl() 
